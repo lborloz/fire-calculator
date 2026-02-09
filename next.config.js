c6563831cc9062a7717001a2dbd8482753aa8fc5
Luke Borloz <luke.borloz@gmail.com>
2026-02-09 16:30:38 -0500

    fix: update basePath configuration in next.config.js and add CNAME file for domain

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/fire-calculator' : '',
+  basePath: '',
 }
 
 module.exports = nextConfig
